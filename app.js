6c469d885becdcd4b25cd01ebf2ab6983dcfe12e
Sarita Singh <saritasingh.0425@gmail.com>
2023-06-21 13:01:19 +0530

    Maintain a better file structur
    Also implemented params middleware in order to check id

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // core modules
-const fs = require('fs');
 const express = require('express');
 // 3rd party module
 const morgan=require('morgan');
-
+// our own modules
+const tourRouter=require('./routes/tourRoutes');
+const userRouter=require('./routes/userRoutes');
 
 const app = express();
 
@@ -25,160 +26,8 @@ app.use((req, res, next)=>{
     next();
 });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`, 'utf-8'));
-// console.log(tours);
-
-// Route handlers or controllers
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        requestTime: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-};
-
-const getTour = (req, res) => {
-    console.log(req.params);
-
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    res.status(200).json({
-        status: 'success',
-        // results: tours.length,
-        data: {
-            tour
-        }
-    });
-}
-
-
-const createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
-}
-
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<updated tour here...>'
-        }
-    });
-}
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-}
-
-const getAllUsers = (req, res) => {
-    res.status(400).json({
-        status: 'error',
-        requestTime: req.requestTime,
-        message: 'This route handler is not yet implemented'
-    });
-};
-
-const getUser = (req, res) => {
-    res.status(400).json({
-        status: 'error',
-        requestTime: req.requestTime,
-        message: 'This route handler is not yet implemented'
-    });
-}
-
-
-const createUser = (req, res) => {
-    res.status(400).json({
-        status: 'error',
-        requestTime: req.requestTime,
-        message: 'This route handler is not yet implemented'
-    });
-}
-
-const updateUser = (req, res) => {
-    res.status(400).json({
-        status: 'error',
-        requestTime: req.requestTime,
-        message: 'This route handler is not yet implemented'
-    });
-}
-
-const deleteUser = (req, res) => {
-    res.status(400).json({
-        status: 'error',
-        requestTime: req.requestTime,
-        message: 'This route handler is not yet implemented'
-    });
-}
-
-// Routes
-const tourRouter=express.Router();
-const userRouter=express.Router();
-
-
-tourRouter
-    .route('/')
-    .get(getAllTours)
-    .post(createTour);
-
-tourRouter
-    .route('/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
-
-userRouter
-    .route('/')
-    .get(getAllUsers)
-    .post(createUser);
-
-userRouter
-    .route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
-
+// mounting Routers on the two different routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// start server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port:${port}...`);
-})
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+// controllers
+exports.getAllUsers = (req, res) => {
+    res.status(400).json({
+        status: 'error',
+        requestTime: req.requestTime,
+        message: 'This route handler is not yet implemented'
+    });
+};
+
+exports.getUser = (req, res) => {
+    res.status(400).json({
+        status: 'error',
+        requestTime: req.requestTime,
+        message: 'This route handler is not yet implemented'
+    });
+};
+
+
+exports.createUser = (req, res) => {
+    res.status(400).json({
+        status: 'error',
+        requestTime: req.requestTime,
+        message: 'This route handler is not yet implemented'
+    });
+};
+
+exports.updateUser = (req, res) => {
+    res.status(400).json({
+        status: 'error',
+        requestTime: req.requestTime,
+        message: 'This route handler is not yet implemented'
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    res.status(400).json({
+        status: 'error',
+        requestTime: req.requestTime,
+        message: 'This route handler is not yet implemented'
+    });
+};
