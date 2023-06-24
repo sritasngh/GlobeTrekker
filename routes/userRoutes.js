e3ae46d2be13afa4ab755e0226b33545cc42272f
Sarita Singh <saritasingh.0425@gmail.com>
2023-06-24 17:11:55 +0530

    Create a route and controller for user signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+// User purpose routers
+router.post('/signup', authController.signup);
+
+// REST format routers are more useful for admin purpose
 router
     .route('/')
     .get(userController.getAllUsers)
